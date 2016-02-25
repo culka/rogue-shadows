6b02ec981e0958da588cbbbcd705e524814c569e
Teemu Mäkinen <teemu.m.makinen@students.tut.fi>
2016-02-25 10:10:52 +0200

    Data loading from JSON files

--- gulpfile.js
+++ gulpfile.js
@@ -15,14 +15,21 @@ gulp.task('watch-ts', function() {
     gulp.watch('src/ts/**/*.ts', ['typescript']);
 });
 gulp.task('html', function() {
-    return gulp.src('src/html/*.html')
+    return gulp.src('src/html/**')
     .pipe(gulp.dest(BUILD_DIR));
 });
 gulp.task('watch-html', function() {
-    gulp.watch('src/html/**/*.html', ['html']);
+    gulp.watch('src/html/**', ['html']);
+});
+gulp.task('data', function() {
+    return gulp.src('src/data/**')
+    .pipe(gulp.dest(BUILD_DIR + '/data'));
+});
+gulp.task('watch-data', function() {
+    gulp.watch('src/data/**', ['data']);
 });
 gulp.task('lib', function() {
     return gulp.src('bower_components/rot.js/rot.js')
     .pipe(gulp.dest(BUILD_DIR + '/lib'));
 });
-gulp.task('default', ['typescript', 'html', 'lib', 'watch-ts', 'watch-html']);
+gulp.task('default', ['typescript', 'html', 'data', 'lib', 'watch-ts', 'watch-html', 'watch-data']);
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,11 +1,30 @@
 ///<reference path="interface/messaging.ts"/>
 
+interface Test {
+    asdf: number;
+}
+
+function loadJSONFromURL<T>(location: string, callback: (result: Array<T>) => void): void {
+
+    let request = new XMLHttpRequest();
+    request.onload = function() {
+        let result: Array<T> = JSON.parse(this.responseText);
+        callback(result);
+    };
+    request.open("get", location, true);
+    request.send();
+}
+
 
 function hello(text: string): void {
     let msg = new Messaging();
 
     msg.displayMessage(text);
     msg.displayMessage(text);
+
+    loadJSONFromURL<Test>("data/test.json", function(items: Array<Test>): void {
+        console.log(items);
+    });
 }
 
 hello("hello");
